41248f2fed049d6e74229e2911878b4cc1e35d35
dodongja <96415700+dodongja@users.noreply.github.com>
2022-05-27 22:58:05 +0900

    Delete backend/src/main/java/com/example/demo/test directory

--- backend/src/main/java/com/example/demo/test/test.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.test;
-
-public class test {
-    //test
-    //test12
-    //test123
-}
--- backend/src/main/java/com/example/demo/test/testcontroller.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.test;
-
-public class testcontroller {
-    /* test 입니다. */
-    /* test2 입니다 */
-    /* test3 입니다 */
-}
