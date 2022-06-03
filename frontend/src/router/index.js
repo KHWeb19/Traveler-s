df0dd0756cb7bd2c0e26aa3b69b2283ea5e6c13e
dodongja <qs553@nate.com>
2022-06-03 21:18:40 +0900

    [이동혁] router 수정

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
-import LogIn from '@/views/member/LogIn.vue'
+
 
 Vue.use(VueRouter)
 
@@ -15,16 +15,8 @@ const routes = [
   {
     path: '/about',
     name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
   },
-  {
-    path: '/login',
-    name: 'LogIn',
-    component: LogIn
-  }
 ]
 
 const router = new VueRouter({
