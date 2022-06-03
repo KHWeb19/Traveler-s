55ed630cfec5529cb3c8c39a33b1bd1fb937e142
dodongja <96415700+dodongja@users.noreply.github.com>
2022-06-03 12:49:04 +0900

    Update index.js

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,11 +1,8 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
-<<<<<<< HEAD
-=======
 //import createPersistedState from 'vuex-persistedstate'
 
->>>>>>> 67e3d113041979b3459d984721929f3756e001a6
 import state from './states'
 import actions from './actions'
 import mutations from './mutations'
@@ -21,10 +18,6 @@ export default new Vuex.Store({
   state,
   actions,
   mutations,
-<<<<<<< HEAD
-  getters
-=======
   getters,
  // plugins
->>>>>>> 67e3d113041979b3459d984721929f3756e001a6
 })
