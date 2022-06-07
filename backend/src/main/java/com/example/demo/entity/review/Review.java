e4b42de803c9a342e7d617ae247044e457eaee41
nuobasic <nuobasic@naver.com>
2022-06-07 21:55:54 +0900

    [최영남] review entity @CreationTimestamp 어노테이션 변경

--- backend/src/main/java/com/example/demo/entity/review/Review.java
+++ backend/src/main/java/com/example/demo/entity/review/Review.java
@@ -1,10 +1,13 @@
 package com.example.demo.entity.review;
 
+import com.example.demo.entity.Hotel.Hotel;
+import com.example.demo.entity.member.User;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.annotation.CreatedDate;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
@@ -27,18 +30,24 @@ public class Review {
     @Lob
     private  String rating;
 
-    @CreationTimestamp
+    @CreatedDate
     private String regDate = LocalDateTime.now().format(DateTimeFormatter.ofLocalizedDateTime(FormatStyle.SHORT));
 
     @UpdateTimestamp
     private Date updDate;
 
     // 외래키
-    private Long userNo;
+    /**
+    @ManyToOne
+    private User user;
 
+    */
     // 외래키
-    private Long hotelNo;
-
+    /**
+    @ManyToOne
+    @JoinColumn(name = "hotelNo")
+    private Hotel hotel;
+*/
 
 
 
