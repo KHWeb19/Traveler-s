8e68cf87580cce9d85014beeb15bde93a5b5e0ef
HyeonJi-Son <sustka46@gmail.com>
2022-06-07 23:14:43 +0900

    [손현지] 사업자 - 예약 현황 확인 ui 시작 [RCU]

---
+++ frontend/src/components/business/ManageReservForm.vue
@@ -0,0 +1,80 @@
+<template>
+    <v-container>
+
+        <template>
+            <v-col>
+                <v-row>
+                    <v-text-field
+                    class="search"
+                    v-model="keyWord"
+                    label="Search"
+                    placeholder="작성자를 검색해주세요."
+                    single-line
+                    hide-details
+                    ></v-text-field>
+                    <v-col cols="2" md="1">
+                        <v-btn id="searchBtn" @click="search" dark small>
+                            <v-icon>
+                                mdi-magnify
+                            </v-icon>
+                        </v-btn>
+                    </v-col>
+                </v-row>
+            </v-col>
+        </template>
+    <br>
+
+    <template>
+        <div class="b_table">
+            <table style="width: 80%">
+                <th>
+                    <td>
+                        <!--날짜-->
+                    </td>
+                    <td>
+                        <!--객실명-->
+                    </td>
+                    <td>
+                        <!--고객명-->
+                    </td>
+                    <td>
+                        <!--숙박인원-->
+                    </td>
+                    <td>
+                        <!--상세보기/체크아웃 버튼-->
+                    </td>
+                </th>
+                <th>
+                    <td>
+                        <!-- -->
+                    </td>
+                </th>
+            </table>
+        </div>
+    </template>
+
+    <!--테이블 페이지네이션-->
+
+    </v-container>
+</template>
+
+<script>
+
+
+export default {
+    name: 'ManageReservForm',
+    components: {
+        
+    }    
+}
+</script>
+
+
+<style scoped>
+.table, th, td{
+    border: 1px solid #444444;
+}
+#search{
+    width:80%;
+}
+</style>
---
+++ frontend/src/views/business/ManageReservPage.vue
@@ -0,0 +1,24 @@
+<template>
+    <div align="center">
+
+        <manage-reserv-form/>
+    </div>
+</template>
+
+<script>
+import ManageReservForm from '@/components/business/ManageReservForm.vue'
+
+
+export default {
+    name: 'ManageReservPage',
+    components: {
+        ManageReservForm
+
+    }    
+}
+</script>
+
+
+<style scoped>
+
+</style>
