446959a2f551e2beaa6e3997136f9c90a01c85f3
asalwaysineedu <lso7182@naver.com>
2022-06-07 20:24:10 +0900

    [이상아] 숙소 관리 시스템 hotel info entity [HMS]

--- backend/src/main/java/com/example/demo/entity/Hotel/HotelInfo.java
+++ backend/src/main/java/com/example/demo/entity/Hotel/HotelInfo.java
@@ -2,9 +2,48 @@ package com.example.demo.entity.Hotel;
 
 import lombok.Data;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
 
 @Data
 @Entity
 public class HotelInfo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String infoId;
+
+    @Column
+    private int oceanView;
+
+    @Column
+    private int terrace;
+
+    @Column
+    private int swimmingPool;
+
+    @Column
+    private int carCharge;
+
+    @Column
+    private int nearAirport;
+
+    @Column
+    private int golfCourse;
+
+    @Column
+    private int freeParking;
+
+    @Column
+    private int bbqGrill;
+
+    @Column
+    private int pet;
+
+    @Column
+    private int pension;
+
+    @Column
+    private int spa;
+
+    @Column
+    private int ski;
 }
