d7c0103a686df03f40c729e676ba542ecae4c08a
asalwaysineedu <lso7182@naver.com>
2022-06-08 19:55:16 +0900

    [이상아] hotel register ui Implementing [HMS]

---
+++ frontend/src/components/Hotel/HotelRegisterForm.vue
@@ -0,0 +1,22 @@
+<template>
+<form @submit.prevent="onSubmit">
+    <label for="hotelName">숙소명</label>
+    <input type="text" v-model="hotelName"/>
+</form>
+</template>
+
+<script>
+export default {
+    name: 'HotelRegisterForm',
+    data () {
+        return {
+            hotelName: '숙소명을 입력해주세요.',
+            
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ frontend/src/views/Hotel/HotelRegisterPage.vue
@@ -0,0 +1,39 @@
+<template>
+  <!-- 숙소 등록 페이지-->
+  <div>
+      <p>*은 필수 입력사항입니다.</p>
+  </div>
+</template>
+
+<script>
+
+import HotelRegisterForm from '@/components/Hotel/HotelRegisterForm.vue'
+import axios from 'axios'
+
+export default {
+    name: 'HotelRegisterPage',
+    components: {
+        HotelRegisterForm
+    },
+    methods: {
+        onSubmit (payload) {
+            const {} = payload
+            axios.post('http://localhost:8080/hotel/hotelregister', {})
+            .then(() => {
+                alert('등록 되었습니다.')
+
+                this.$router.push({
+                    name: 'HotelListPage'
+                })
+            })
+            .catch(() => {
+                alert('오류가 발생하였습니다.')
+            })
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
