2b632cd64cb3387811474a2cac1bc7fb9da1af7d
HyeonJi-Son <sustka46@gmail.com>
2022-06-08 21:55:31 +0900

    [손현지] 테이블 기본세팅 [RCU]

--- frontend/src/components/business/ManageReservForm.vue
+++ frontend/src/components/business/ManageReservForm.vue
@@ -27,28 +27,40 @@
     <template>
         <div class="b_table">
             <table style="width: 80%">
-                <th>
-                    <td>
-                        <!--날짜-->
+                <tr>
+                    <th align="center" width="100">날짜</th>
+                    <th align="center" width="540">객실명</th>
+                    <th align="center" width="150">고객명</th>
+                    <th align="center" width="150">숙박인원</th>
+                    <th align="center" width="130">  </th>
+                </tr>
+
+                <tr v-if="!reservs || (Array.isArray(reservs) && reservs.length === 0)">
+                    <td colspan="5">
+                        현재 예약 고객이 없습니다!
                     </td>
-                    <td>
-                        <!--객실명-->
+                </tr>
+                <tr v-else v-for="reserv in reservs" :key="reserv.reservNo">
+                    <td align="center"> <!-- 날짜 -->
+                        {{ reserv.regDate }}
                     </td>
-                    <td>
-                        <!--고객명-->
+
+                    <td align="left"> <!-- 객실명 -->
+                        {{ reserv.room_name}}
                     </td>
-                    <td>
-                        <!--숙박인원-->
+                    <td align="right"> <!-- 고객명 -->
+                        {{ reserv.name }}
                     </td>
-                    <td>
-                        <!--상세보기/체크아웃 버튼-->
+                    <td  align="center"> <!-- 숙박인원 -->
+                        {{ reserv.personnel }}
                     </td>
-                </th>
-                <th>
-                    <td>
-                        <!-- -->
+                    <td align="center">
+                        <router-link :to="{ name: 'JpaBoardReadPage',
+                                            params: { boardNo: reserv.reservNo.toString() } }">
+                            <v-btn>상세보기</v-btn>
+                        </router-link>
                     </td>
-                </th>
+                </tr>                
             </table>
         </div>
     </template>
