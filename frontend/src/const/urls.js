d86f1f46a9eddac97d8cd2f37e1f783acae66f72
thkim750 <thkim750@gmail.com>
2022-06-12 11:08:09 +0900

    로그인 UI 작업중

--- frontend/src/const/urls.js
+++ frontend/src/const/urls.js
@@ -2,6 +2,6 @@ export const BASE_URL = "http://localhost:7777"
 
 export const REDIRECT_URL = "http://localhost:8080/oauth2/redirect"
 
-export const GOOGLE_URL = BASE_URL + "/oauth2/authorize/google?redirect_url=" + REDIRECT_URL
+export const GOOGLE_URL = BASE_URL + "/oauth2/authorize/google?redirect_uri=" + REDIRECT_URL
 
-export const NAVER_URL = BASE_URL + "/oauth2/authorize/naver?redirect_url=" + REDIRECT_URL
+export const NAVER_URL = BASE_URL + "/oauth2/authorize/naver?redirect_uri=" + REDIRECT_URL
