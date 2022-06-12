4f5033aeddf585521dc664abb80b6b44219188fb
dodongja <96415700+dodongja@users.noreply.github.com>
2022-06-12 11:42:38 +0900

    Revert "[김태현] login ui 작업중"

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -17,16 +17,6 @@ const routes = [
     name: 'about',
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
   },
-  {
-    path: '/login',
-    name: 'login',
-    component: () => import('../views/member/MemberLogin.vue')
-  },
-  {
-    path: '/loginEmail',
-    name: 'loginEmail',
-    component: () => import('../views/member/MemberLoginEmail.vue')
-  }
 ]
 
 const router = new VueRouter({
