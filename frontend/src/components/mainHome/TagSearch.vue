f8919d004bda4ac6823ba12a8b67c5cec626bd61
HyeonJi-Son <sustka46@gmail.com>
2022-06-17 15:28:39 +0900

    [손현지]0616 오류수정

---
+++ frontend/src/components/mainHome/TagSearch.vue
@@ -0,0 +1,29 @@
+<template>
+    <v-container fluid>
+        <section>
+            <div>
+                <v-card-title class="justify-center" style="font-size:2.5em">테마 검색</v-card-title>
+                <div>
+                    <v-btn rounded>(#오션뷰)</v-btn>
+                    <v-btn rounded>(#테라스)</v-btn>
+                    <v-btn rounded>(#수영장)</v-btn>
+                    <v-btn rounded>(#전기차충전)</v-btn>
+                    <v-btn rounded>(#공항근처)</v-btn>
+                    <v-btn rounded>(#골프장)</v-btn>
+                    <v-btn rounded>(#무료주차)</v-btn>
+                    <v-btn rounded>(#바베큐그릴)</v-btn>
+                    <v-btn rounded>(#반려동물)</v-btn>
+                    <v-btn rounded>(#온천)</v-btn>
+                </div>
+            </div>
+        </section>
+    </v-container>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+
+</style>
