257a1fd057c9a7297bb49dd1caae14e806c5022e
dodongja <qs553@nate.com>
2022-06-25 06:55:19 +0900

    [이동혁]kakaoApi 오류 수정

--- frontend/src/components/hotelDetail/KakaoMapApi.vue
+++ frontend/src/components/hotelDetail/KakaoMapApi.vue
@@ -10,7 +10,8 @@ export default {
     name: 'KakaoMap',
     props : {
         mHotel:{
-            type:Array
+            type: Object,
+            required: true
         }
     },
     data() {
@@ -29,9 +30,9 @@ export default {
             const map = new kakao.maps.Map(container, options)
             this.geocoder = new kakao.maps.services.Geocoder()
             console.log(this.geocoder)
-            
-            const hotelName = this.mHotel.hotelName
-            this.geocoder.addressSearch(this.mHotel.totalAddress, function(result, status) {
+            console.log('init' + this.hotel)
+            const hotel = this.mHotel
+            this.geocoder.addressSearch(hotel.totalAddress, function(result, status) {
                 console.log(kakao.maps.services.Status.OK)
                 if (status === kakao.maps.services.Status.OK) {
                       
@@ -47,7 +48,7 @@ export default {
                     // 인포윈도우로 장소에 대한 설명을 표시합니다
                     const infowindow = new kakao.maps.InfoWindow({
                        
-                        content: `<div style="width:150px;text-align:center;padding:6px 0;">${hotelName} </div>`
+                        content: `<div style="width:150px;text-align:center;padding:6px 0;">${hotel.hotelName} </div>`
                     })
                     infowindow.open(map, marker)
 
@@ -57,15 +58,17 @@ export default {
             });    
         }
     },
-    mounted() {
-        if(!window.kakao || !window.kakao.maps){
-            const script = document.createElement("script")
-             /* global kakao */
-            script.onload = () => kakao.maps.load(this.initMap);
-            script.src= "https://dapi.kakao.com/v2/maps/sdk.js?appkey=ec61fc18851964c845de3db938cfd080&libraries=services&autoload=false"
-            document.head.appendChild(script);
-        }else{
-            this.initMap()
+    watch: {
+        mHotel: function() {
+            if(!window.kakao || !window.kakao.maps){
+                const script = document.createElement("script")
+                /* global kakao */
+                script.onload = () => kakao.maps.load(this.initMap);
+                script.src= "https://dapi.kakao.com/v2/maps/sdk.js?appkey=ec61fc18851964c845de3db938cfd080&libraries=services&autoload=false"
+                document.head.appendChild(script);
+            }else{
+                this.initMap()
+            }
         }
     }
 }
