40f55a5ed76fae1c0e9e9a69129cfd92a08b4541
dodongja <qs553@nate.com>
2022-06-30 05:58:45 +0900

    [이동혁] tagSearch dto 작업

--- backend/src/main/java/com/example/demo/service/search/SearchService.java
+++ backend/src/main/java/com/example/demo/service/search/SearchService.java
@@ -1,11 +1,12 @@
 package com.example.demo.service.search;
 
+import com.example.demo.dto.hotel.HotelResponse;
 import com.example.demo.dto.search.KeyWordRequest;
 import com.example.demo.entity.hotel.Hotel;
 
 import java.util.List;
 
 public interface SearchService {
-    public List<Hotel> tagSearchList(String word);
+    public List<HotelResponse> tagSearchList(String word);
     public List<Hotel> commonSearchList(KeyWordRequest keyWordRequest);
 }
