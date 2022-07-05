7bebd3baee129e57ed4e2a3cff7e6b802d56eb5d
HyeonJi-Son <sustka46@gmail.com>
2022-07-05 20:25:38 +0900

    [손현지] 사용자 - 예약내역 확인 UI 완료 [APU]

--- frontend/src/views/mypage/MyPageBooking.vue
+++ frontend/src/views/mypage/MyPageBooking.vue
@@ -1,29 +1,62 @@
 <template>
-  <v-container style="text-align: center">
-    <div style="display: inline-block">
-      <v-row>
-        <v-col>
-          <my-page-left-menu :userInfo="user"></my-page-left-menu>
-        </v-col>
-        <v-col>
-          <my-page-booking-list
-            :bookingLists="bookingLists"
-          ></my-page-booking-list>
-        </v-col>
-      </v-row>
-    </div>
+  <div align="center" style="display: inline-block">
+    <v-container>
+
+      <table style="width: 800px;">
+        <tr>
+          <td align="left" colspan="2">
+            <h2 class="pageTit">나의 예약 내역</h2>
+          </td>
+        </tr>
+
+        <tr>
+          <td align="left">
+            <span class="page-count">전체</span>
+          </td>
+          <td align="right">
+            <label class="reservationNameLabel">* 예약상태</label>
+            <select
+              class="reservation-select"
+              v-model="id"
+              @change="getStatusList($event)">
+              
+              <option :value="''"> 예약상태를 선택하세요.</option>
+        <!--      <option
+                v-for="(reservation, idx) in status"
+                :key="idx"
+                :value="reservation.id">
+                {{ reservation.status }}
+              </option> -->
+            </select>
+          </td>
+        </tr>
+
+        <tr>
+          <td colspan="2">
+            <v-row>
+              <v-col>
+                <my-page-booking-list
+                  :bookingLists="bookingLists"
+                ></my-page-booking-list>
+              </v-col>
+            </v-row>
+          </td>
+        </tr>
+      </table>
+
   </v-container>
+  </div>
 </template>
 
 <script>
-import MyPageLeftMenu from "@/components/mypage/MyPageLeftMenu.vue";
+//import MyPageLeftMenu from "@/components/mypage/MyPageLeftMenu.vue";
 import MyPageBookingList from "@/components/mypage/MyPageBookingList.vue";
 import { mapActions, mapState } from "vuex";
 
 export default {
   name: "MyBookingList",
   components: {
-    MyPageLeftMenu,
+    //MyPageLeftMenu,
     MyPageBookingList,
   },
 
@@ -31,8 +64,16 @@ export default {
     ...mapState(["user", "bookingLists"]),
   },
   methods: {
+    ...mapActions(["getStatusType"]),
     ...mapActions(["setUser", "fetchBookingLists"]),
-  },
+
+    getRoomList(event) {
+      this.id = event.target.value;
+      this.fetchBmRoomList(this.id)
+      
+    },
+
+},
   mounted() {
     this.setUser();
     this.fetchBookingLists();
@@ -40,4 +81,66 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+/*table, th, td{
+    border-collapse:collapse;
+    border: 1px solid black;
+}*/
+
+div {
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+}
+h2 {
+  padding: 50px 0px 30px 0px;
+}
+.pageTit {
+  padding: 50px 0px 30px 0px;
+  font-family: 'NanumSquareRound';
+  font-size: 2.0rem;
+  line-height: 1.25;
+  letter-spacing: -.01em;
+  color: #202020;
+  font-weight: 900;
+}
+.pageTit:after {
+    content: '';
+    display: inline-block;
+    width: 4px;
+    height: 4px;
+    margin-bottom: 24px;
+    border-radius: 50%;
+    background-color: #e63668;
+}
+.page-count {
+  margin-left: 15px;
+  border: 1px;
+  border-bottom: 5px solid #e63668;
+  border-radius: 5px;
+  padding: 5px 10px;
+  outline: none;
+  color: #18225c;
+  -webkit-text-stroke: 0.7px #18225c;
+}
+.btn {
+  position: sticky;
+  left: 90%;
+  text-decoration: none;
+}
+.reservation-select {
+  margin-top: 10px;
+  margin-left: 30px;
+  width: 200px;
+  height: 30px;
+  border-radius: 2pt;
+  box-shadow: 0 0 0 1pt grey;
+  outline: none;
+  transition: 0.1s;
+}
+.reservationNameLabel {
+  font-size: 14px;
+  margin-left: 50px;
+}
+</style>
