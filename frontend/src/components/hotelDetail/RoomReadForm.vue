1928eb817040b5e8d52dbadfa3f6815db566d5dd
dodongja <qs553@nate.com>
2022-07-08 14:16:35 +0900

    [이동혁] push안됨

--- frontend/src/components/hotelDetail/RoomReadForm.vue
+++ frontend/src/components/hotelDetail/RoomReadForm.vue
@@ -245,6 +245,11 @@ export default {
         
     },
   mounted () {
+    this.roomList = this.$store.state.mRooms
+    console.log(this.mRooms)
+    console.log('room')
+    console.log(this.roomList)
+    console.log(this.$store.state.mRooms)
     this.dates = this.payload.dates
     this.personnel = this.payload.personnel
     
